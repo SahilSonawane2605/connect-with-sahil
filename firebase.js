42989a5060009117f9291bb30d5a74e1432e7fcb
Sahil Sonawane <sahil.sonawane.26005@gmail.com>
2026-06-09 10:48:59 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -15,10 +15,9 @@ storageBucket: "sage-connect-2d2e0.firebasestorage.app",
 messagingSenderId: "831286879311",
 appId: "1:831286879311:web:4938aca6bece033c4fb63a"
 };
-
+console.log("Firebase Connected Successfully");
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 
 window.saveInquiry = async function(data) {
 
