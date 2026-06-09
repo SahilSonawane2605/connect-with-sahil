c22a48bf1b371e38b6f68344713f6a863017c9e9
Sahil Sonawane <sahil.sonawane.26005@gmail.com>
2026-06-09 10:17:47 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,36 +1,51 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
 import {
-  getFirestore,
-  collection,
-  addDoc,
-  serverTimestamp
+getFirestore,
+collection,
+addDoc,
+serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDulNyUchsCTBAw0Smuc5nK7wzJIE_Cz2A",
-  authDomain: "sage-connect-2d2e0.firebaseapp.com",
-  projectId: "sage-connect-2d2e0",
-  storageBucket: "sage-connect-2d2e0.firebasestorage.app",
-  messagingSenderId: "831286879311",
-  appId: "1:831286879311:web:4938aca6bece033c4fb63a"
+apiKey: "AIzaSyDulNyUchsCTBAw0Smuc5nK7wzJIE_Cz2A",
+authDomain: "sage-connect-2d2e0.firebaseapp.com",
+projectId: "sage-connect-2d2e0",
+storageBucket: "sage-connect-2d2e0.firebasestorage.app",
+messagingSenderId: "831286879311",
+appId: "1:831286879311:web:4938aca6bece033c4fb63a"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-console.log("Firebase Connected");
-console.log("Saving:", data);
+
+console.log("Firebase Connected Successfully");
 
 window.saveInquiry = async function(data) {
-  try {
-    await addDoc(collection(db, "inquiries"), {
-      ...data,
-      createdAt: serverTimestamp()
-    });
 
-    return true;
-  } catch (err) {
-    console.error(err);
-    return false;
+console.log("Saving inquiry...", data);
+
+try {
+
+```
+await addDoc(
+  collection(db, "inquiries"),
+  {
+    ...data,
+    createdAt: serverTimestamp()
   }
+);
+
+return true;
+```
+
+} catch (err) {
+
+```
+console.error(err);
+
+return false;
+```
+
+}
 };
